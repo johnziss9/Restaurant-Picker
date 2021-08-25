72fcb8b3cbacf01ea7876484f2f9b3ddd514ec57
John Zissimou <johnzissimou@johns-mbp.home>
2021-08-25 09:47:06 +0100

    Setup secrets file and settings. Created models for Restaurant and Cuisines.. Installed MongoDBDriver.

---
+++ Models/Cuisine.cs
@@ -0,0 +1,28 @@
+namespace Restaurant_Picker.Models
+{
+    public enum Cuisine
+    {
+        American = 1,
+        Birtish = 2,
+        Chinese = 3,
+        Japanese = 4,
+        German = 5,
+        Korean = 6,
+        Italian = 7,
+        Indian = 8,
+        Greek = 9,
+        Turkish = 10,
+        Thai = 11,
+        Spanish = 12,
+        Czech = 13,
+        Mexican= 14,
+        Vietnamese = 15,
+        Portugese = 16,
+        Brazilian = 17,
+        Polish = 18,
+        French = 19,
+        Other = 20,
+        Irish = 21,
+        Scottish = 22
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,21 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Restaurant_Picker.Models
+
+{
+    public class Restaurant
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Cuisine Cuisine { get; set; }
+        public string Location { get; set; }
+        public bool Visited { get; set; } = false;
+        public bool Deleted { get; set; } = false;
+        public DateTime AddedOn { get; set; } = DateTime.Now;
+        public DateTime DateVisited { get; set; } = DateTime.Now;
+    }
+}
